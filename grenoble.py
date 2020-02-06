42359e81b39176838fc9cc6036ad02a13e8c2f0c
hightc.phy.hr <ijakovac@phy.hr>
2020-02-06 09:52:57 +0100

    "first code commit"

---
+++ grenoble.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+__author__ = "ijakovac@phy.hr"
+
+"""
+Created on Thu Jan 16 09:35:39 2020
+
+@author: Ivan
+"""
+
+from tecmag import TNTReader
+import numpy as np
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, 340, 120)
+        self.setWindowTitle('Zagreb to Grenoble')
+        self.statusBar()
+
+
+        self.label = QtWidgets.QLineEdit(self)
+        self.label.setText('... no file selected ...')
+        self.label.move(20,20)
+        self.label.resize(300,20)
+
+        btnSelectFile = QtWidgets.QPushButton('Select .tnt file', self)
+        btnSelectFile.setGeometry(QtCore.QRect(80,50,180,30))
+        btnSelectFile.clicked.connect(self.browse)
+
+        self.file = ''
+
+    def browse(self):
+        self.statusBar().setStyleSheet("QStatusBar{padding-left:8px;background:rgba(255,0,0,255);color:black;font-weight:bold;}")
+        self.statusBar().showMessage('Translating...')
+        self.file = QtWidgets.QFileDialog.getOpenFileName(self, 'Select file to translate', filter='*.tnt')[0]
+        self.label.setText(self.file)
+        #try:
+        self.translate()
+        #except:
+        #    QtWidgets.QMessageBox.critical(self, "Critical error", "Unsupported file.", QtWidgets.QMessageBox.Ok)
+        self.statusBar().setStyleSheet("QStatusBar{padding-left:8px;background:rgba(0,255,0,255);color:black;font-weight:bold;}")
+        self.statusBar().showMessage('File translated!')
+
+
+    def translate(self):
+        tnt = TNTReader(self.file)
+        
+        with open(self.file.replace('.tnt', '.dat'), 'w') as output:
+            # header, comments and details
+            output.write('%%\nPython translator\nFile translated from Tecmag spectrometer (TNT format)\n%End%\n\n')
+            output.write('%Program Comments%\nPulse prog comments\n%End%\n\n')
+            output.write('%Pulse Program%\nPulse prog details\n%End%\n\n')
+        
+            # PhaseList
+            output.write('%Phase Lists%\nTl1\nTl2\nTl3\nRl1\nRl2\n%End%\n\n')
+        
+            # Number of skipped transfers
+            output.write('%nb of skipped transfers%\n0\n%End%\n\n')
+        
+            # Loops
+            output.write('%loops%\n1\t0\n1\t0\n%End%\n\n')
+        
+            # Delays
+            output.write('%Delays%\nAcq.time\t{0:.1f}u\t0\n'.format(float(tnt.params['acq_time'])*10**6))
+            for i in range(5): output.write('\t\t0\n')
+            output.write('%End%\n\n')
+        
+            # Parameters
+            output.write('%Parameters%\n')
+            output.write('Frequency\t{0:.6f} MHz\t{1}\n'.format(tnt.params['ob_freq'][0], tnt.params['obs_channel']))
+            output.write('Field\t{0:.5f} T\t0\n'.format(tnt.params['magnet_field']))
+            output.write('DW\t{0:.3f} us\t0\n'.format(float(tnt.params['dwell'][0])*10**6))
+            output.write('Sensitivity\t? V\t0\n')
+            output.write('Scans\t{}\t0\n'.format(int(tnt.params['scans'])))
+            output.write('Transfer\t{}\t0\n'.format(int(tnt.params['actual_scans'])))
+            output.write('Record size\t{}\t0\n'.format(int(tnt.params['acq_points'])))
+            output.write('Power\t0 dB\t0\n')
+            output.write('Aux. frequency\t{0:.2f} MHz\t0\n'.format(float(tnt.params['ref_freq'])))
+            output.write('Temperature\t{0:.1f} K\t0\n'.format(int(tnt.params['actual_temperature'])))
+            output.write('%End%\n\n')
+        
+            # Variables
+            # Identify a type of measurement
+            for i in range(tnt.params['NCols']):
+                if tnt.params['Sequence']['F1_Freq'][i][3] != '':
+                    typ = 'fsw'
+                    table = tnt.params['Sequence']['F1_Freq'][i][3] # index [3] is a 2D table
+                elif tnt.params['Sequence']['F1_Atten'][i][3] != '':
+                    typ = 'att'
+                    table = tnt.params['Sequence']['F1_Atten'][i][3]
+                elif tnt.params['Sequence']['Delay'][i][3] != '':
+                    typ = 't1'
+                    table = tnt.params['Sequence']['Delay'][i][3]
+        
+            output.write('%Variables : level1%\n')
+            if typ == 'fsw' or typ == 'att':
+                output.write('Frequency\t')
+                frequencies = ['{0:.6f} MHz'.format(tnt.params['ob_freq'][0]+int(offset)/10**6) for offset in tnt.params['Tables'][table]]
+                output.write('\t'.join(frequencies))
+            elif typ == 't1':
+                output.write('delta\t')
+                output.write('\t'.join(tnt.params['Tables'][table]).replace('u', ' us').replace('m', ' ms'))
+            else:
+                output.write('Frequency\t')
+                output.write('{0:.6f} MHz'.format(tnt.params['ob_freq'][0]))
+            output.write('\n\tNone\n%End%\n\n')
+        
+            output.write('%Variables : level2%\n\tNone\n\tNone\n%End%\n\n')
+            output.write('%Nb max of Measurements%\n{}\n%End%\n\n'.format(tnt.data.shape[1]))
+            output.write('%Comments on experiment%\nExp Comments\n%End%\n\n')
+        
+            # Data output
+            output.write('%Data%\n')
+            data = np.array(tnt.data)
+            for measurement in range(data.shape[1]):
+                output.write('measure {}\n'.format(measurement+1))
+                output.write('scans {}\n'.format(int(tnt.params['actual_scans'])))
+                reals = ['{0:.3E}'.format(float(np.real(data[data_point, measurement]))) for data_point in range(tnt.data.shape[0])]
+                imags = ['{0:.3E}'.format(float(np.imag(data[data_point, measurement]))) for data_point in range(tnt.data.shape[0])]
+                output.write('\t'.join(reals))
+                output.write('\n')
+                output.write('\t'.join(imags))
+                output.write('\n')
+
+if __name__ == '__main__':
+    if not QApplication.instance():
+        app = QApplication(sys.argv)
+    else:
+        app = QApplication.instance()
+    app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
+    GUI = Window()
+    GUI.show()
+    sys.exit(app.exec_())
---
+++ tecmag.py
@@ -0,0 +1,400 @@
+"""
+Functions for reading Tecmag .tnt data files.
+"""
+
+__author__ = "ijakovac@phy.hr"
+__developer_doc__ = """
+Tecmag .tnt file format information
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+The Tecmag .tnt file format is documented with C pseudo-code in the file
+"A1 - TNMR File Format.doc" distributed with the TNMR software.
+
+This file is based on the
+`pytnt module <https://www.github.com/chatcannon/pytnt>`_.
+Please inform upstream if you find a bug or to request additional features.
+
+This file was modified to read Sequence part of .tnt file (PSEQ tag).
+
+"""
+
+import re
+import io
+
+import numpy as np
+
+class BytesIO2(io.BytesIO):
+    # upgrade to standard io.BytesIO class to read strings with preceding UInt32 lengths
+    def __init__(self):
+        super()
+
+    def read_string(self):
+        length = np.frombuffer(self.read(4), '<u4')[0]
+        return np.frombuffer(self.read(length), 'S{}'.format(length))[0] if length else b''
+
+class TNTReader():
+
+    TNTMAGIC_RE = re.compile(b"^TNT1\.\d\d\d$")
+    
+    TNTMAGIC = np.dtype('S8')
+    TNTTLV = np.dtype([('tag', 'S4'), ('bool', '<u4'), ('length', '<u4')])
+    
+    TNTTMAG = np.dtype([
+        ('npts', '<i4', 4),
+        ('actual_npts', '<i4', 4),
+        ('acq_points', '<i4'),
+        ('npts_start', '<i4', 4),
+        ('scans', '<i4'),
+        ('actual_scans', '<i4'),
+        ('dummy_scans', '<i4'),
+        ('repeat_times', '<i4'),
+        ('sadimension', '<i4'),
+        ('samode', '<i4'),
+    
+        ('magnet_field', '<f8'),
+        ('ob_freq', '<f8', 4),
+        ('base_freq', '<f8', 4),
+        ('offset_freq', '<f8', 4),
+        ('ref_freq', '<f8'),
+        ('NMR_frequency', '<f8'),
+        ('obs_channel', '<i2'),
+        ('space2', 'V42'),
+    
+        ('sw', '<f8', 4),
+        ('dwell', '<f8', 4),
+        ('filter', '<f8'),
+        ('experiment_time', '<f8'),
+        ('acq_time', '<f8'),
+        ('last_delay', '<f8'),
+        ('spectrum_direction', '<i2'),
+        ('hardware_sideband', '<i2'),
+        ('Taps', '<i2'),
+        ('Type', '<i2'),
+        ('bDigRec', '<u4'),
+        ('nDigitalCenter', '<i4'),
+        ('space3', 'V16'),
+    
+        ('transmitter_gain', '<i2'),
+        ('receiver_gain', '<i2'),
+        ('NumberOfReceivers', '<i2'),
+        ('RG2', '<i2'),
+        ('receiver_phase', '<f8'),
+        ('space4', 'V4'),
+    
+        ('set_spin_rate', '<u2'),
+        ('actual_spin_rate', '<u2'),
+    
+        ('lock_field', '<i2'),
+        ('lock_power', '<i2'),
+        ('lock_gain', '<i2'),
+        ('lock_phase', '<i2'),
+        ('lock_freq_mhz', '<f8'),
+        ('lock_ppm', '<f8'),
+        ('H2O_freq_ref', '<f8'),
+        ('space5', 'V16'),
+    
+        ('set_temperature', '<f8'),
+        ('actual_temperature', '<f8'),
+    
+        ('shim_units', '<f8'),
+        ('shims', '<i2', 36),
+        ('shim_FWHM', '<f8'),
+    
+        ('HH_dcpl_attn', '<i2'),
+        ('DF_DN', '<i2'),
+        ('F1_tran_mode', '<i2', 7),
+        ('dec_BW', '<i2'),
+        ('grd_orientation', 'a4'),
+        ('LatchLP', '<i4'),
+        ('grd_Theta', '<f8'),
+        ('grd_Phi', '<f8'),
+        ('space6', 'V264'),
+    
+        ('start_time', '<u4'),
+        ('finish_time', '<u4'),
+        ('elapsed_time', '<i4'),
+    
+        ('date', 'S32'),
+        ('nuclei', 'S16', 4),
+        ('sequence', 'S32'),
+        ('lock_solvent', 'S16'),
+        ('lock_nucleus', 'S16')
+    ])
+    
+    
+    TNTGRIDANDAXIS = np.dtype([
+        ('majorTickInc', '<f8', 12),
+        ('minorIntNum', '<i2', 12),
+        ('labelPrecision', '<i2', 12),
+        ('gaussPerCentimeter', '<f8'),
+        ('gridLines', '<i2'),
+        ('axisUnits', '<i2'),
+        ('showGrid', '<u4'),
+        ('showGridLabels', '<u4'),
+        ('adjustOnZoom', '<u4'),
+        ('showDistanceUnits', '<u4'),
+        ('axisName', 'S32'),
+        ('space', 'V52'),
+    ])
+    
+    
+    TNTTMG2 = np.dtype([
+        ('real_flag', '<u4'),
+        ('imag_flag', '<u4'),
+        ('magn_flag', '<u4'),
+        ('axis_visible', '<u4'),
+        ('auto_scale', '<u4'),
+        ('line_display', '<u4'),
+        ('show_shim_units', '<u4'),
+    
+        ('integral_display', '<u4'),
+        ('fit_display', '<u4'),
+        ('show_pivot', '<u4'),
+        ('label_peaks', '<u4'),
+        ('keep_manual_peaks', '<u4'),
+        ('label_peaks_in_units', '<u4'),
+        ('integral_dc_average', '<u4'),
+        ('integral_show_multiplier', '<u4'),
+        ('Boolean_space', '<u4', 9),
+    
+        ('all_ffts_done', '<u4', 4),
+        ('all_phase_done', '<u4', 4),
+    
+        ('amp', '<f8'),
+        ('ampbits', '<f8'),
+        ('ampCtl', '<f8'),
+        ('offset', '<i4'),
+    
+        ('axis_set', TNTGRIDANDAXIS),
+    
+        ('display_units', '<i2', 4),
+        ('ref_point', '<i4', 4),
+        ('ref_value', '<f8', 4),
+        ('z_start', '<i4'),
+        ('z_end', '<i4'),
+        ('z_select_start', '<i4'),
+        ('z_select_end', '<i4'),
+        ('last_zoom_start', '<i4'),
+        ('last_zoom_end', '<i4'),
+        ('index_2D', '<i4'),
+        ('index_3D', '<i4'),
+        ('index_4D', '<i4'),
+    
+        ('apodization_done', '<i4', 4),
+        ('linebrd', '<f8', 4),
+        ('gaussbrd', '<f8', 4),
+        ('dmbrd', '<f8', 4),
+        ('sine_bell_shift', '<f8', 4),
+        ('sine_bell_width', '<f8', 4),
+        ('sine_bell_skew', '<f8', 4),
+        ('Trapz_point_1', '<i4', 4),
+        ('Trapz_point_2', '<i4', 4),
+        ('Trapz_point_3', '<i4', 4),
+        ('Trapz_point_4', '<i4', 4),
+        ('trafbrd', '<f8', 4),
+        ('echo_center', '<i4', 4),
+    
+        ('data_shift_points', '<i4'),
+        ('fft_flag', '<i2', 4),
+        ('unused', '<f8', 8),
+        ('pivot_point', '<i4', 4),
+        ('cumm_0_phase', '<f8', 4),
+        ('cumm_1_phase', '<f8', 4),
+        ('manual_0_phase', '<f8'),
+        ('manual_1_phase', '<f8'),
+        ('phase_0_value', '<f8'),
+        ('phase_1_value', '<f8'),
+        ('session_phase_0', '<f8'),
+        ('session_phase_1', '<f8'),
+    
+        ('max_index', '<i4'),
+        ('min_index', '<i4'),
+        ('peak_threshold', '<f4'),
+        ('peak_noise', '<f4'),
+        ('integral_dc_points', '<i2'),
+        ('integral_label_type', '<i2'),
+        ('integral_scale_factor', '<f4'),
+        ('auto_integrate_shoulder', '<i4'),
+        ('auto_integrate_noise', '<f8'),
+        ('auto_integrate_threshold', '<f8'),
+        ('s_n_peak', '<i4'),
+        ('s_n_noise_start', '<i4'),
+        ('s_n_noise_end', '<i4'),
+        ('s_n_calculated', '<f4'),
+    
+        ('Spline_point', '<i4', 14),
+        ('Spline_point_avr', '<i2'),
+        ('Poly_point', '<i4', 8),
+        ('Poly_point_avr', '<i2'),
+        ('Poly_order', '<i2'),
+    
+        ('space', 'V610'),
+    
+        ('line_simulation_name', 'S32'),
+        ('integral_template_name', 'S32'),
+        ('baseline_template_name', 'S32'),
+        ('layout_name', 'S32'),
+        ('relax_information_name', 'S32'),
+        ('username', 'S32'),
+        ('user_string_1', 'S16'),
+        ('user_string_2', 'S16'),
+        ('user_string_3', 'S16'),
+        ('user_string_4', 'S16')
+    ])
+
+    PSEQROW = np.dtype([
+                ('Number of Columns', '<V4'),
+                ('Address', '<V4'),
+                ('BitLength', '<V4'),
+                ('Icon Library Type', 'V4'),
+                ('Visible Flag', '<V4'),
+                ('Private Data', '<V4'),
+                ('Group', '<V4'),
+    ])
+
+
+    def __init__(self, filename):
+        """
+        Read a Tecmag .tnt data file.
+    
+        Parameters
+        ----------
+        filename : str
+            Name of file to read from
+    
+        Returns
+        -------
+        dic : dict
+            Dictionary of Tecmag parameters.
+        data : ndarray
+            Array of NMR data.
+    
+        """
+        self.tnt_sections = dict()
+    
+        with open(filename, 'rb') as tntfile:
+    
+            # Check if .tnt file is valid
+            tntmagic = np.frombuffer(tntfile.read(self.TNTMAGIC.itemsize),
+                                     self.TNTMAGIC, count=1)[0]
+    
+            if not self.TNTMAGIC_RE.match(tntmagic):
+                err = ("Invalid magic number (is '%s' really TNMR file?): %s" %
+                       (filename, tntmagic))
+                raise ValueError(err)
+    
+            # Read in the section headers
+            tnthdrbytes = tntfile.read(self.TNTTLV.itemsize)
+            while(self.TNTTLV.itemsize == len(tnthdrbytes)):
+                tlv = np.frombuffer(tnthdrbytes, self.TNTTLV)[0]
+                data_length = tlv['length']
+                hdrdict = {'offset': tntfile.tell(),
+                           'length': data_length,
+                           'bool': bool(tlv['bool'])}
+                if tlv['tag'].decode() == 'PSEQ':
+                    # PSEQ stucture doesn't have length parameter, so we go back 4 bytes.
+                    tntfile.seek(-4, 1)
+                    hdrdict['data'] = tntfile.read()
+                    hdrdict['length'] = len(hdrdict['data'])
+                else:
+                    hdrdict['data'] = tntfile.read(data_length)
+    
+    
+                self.tnt_sections[tlv['tag'].decode()] = hdrdict
+                tnthdrbytes = tntfile.read(self.TNTTLV.itemsize)
+    
+    
+        assert(self.tnt_sections['TMAG']['length'] == self.TNTTMAG.itemsize)
+        self.tmag = np.frombuffer(self.tnt_sections['TMAG']['data'], self.TNTTMAG, count=1)[0]
+    
+        assert(self.tnt_sections['DATA']['length'] == self.tmag['actual_npts'].prod() * 8)
+    
+        self.data = np.memmap(filename, np.dtype('<c8'), mode='c', offset=self.tnt_sections['DATA']['offset'],
+                         shape=self.tmag['actual_npts'].prod())
+        self.data = np.reshape(self.data, self.tmag['actual_npts'], order='F')
+    
+        assert(self.tnt_sections['TMG2']['length'] == self.TNTTMG2.itemsize)
+        self.tmg2 = np.frombuffer(self.tnt_sections['TMG2']['data'], self.TNTTMG2, count=1)[0]
+
+    
+        self.params = dict()
+        # save TMAG data into dic
+        for name in self.TNTTMAG.names:
+            if not name.startswith('space'):
+                self.params[name] = self.tmag[name]
+        # save TMAG data into dic
+        for name in self.TNTTMG2.names:
+            if name not in ['Boolean_space', 'unused', 'space', 'axis_set']:
+               self.params[name] = self.tmg2[name]
+        for name in self.TNTGRIDANDAXIS.names:
+            if not name.startswith('space'):
+                self.params[name] = self.tmg2['axis_set'][name]
+    
+        # update pseq data
+        self.params.update(self.pseq_read(self.tnt_sections['PSEQ']['data']))
+
+
+    def pseq_read(self, data):
+        dic = dict()
+        binary = BytesIO2()
+        binary.write(data)
+        binary.seek(0)
+
+        dic['SequenceID'] = np.frombuffer(binary.read(8), 'S8')[0].decode()
+        dic['File Name'] = binary.read_string()
+        binary.seek(8, 1)
+    
+        if dic['SequenceID'][:4] == '1.18':
+            dic['E-mail'] = binary.read_string()
+            binary.read_string()
+    
+        dic['NRows'] = np.frombuffer(binary.read(4), '<u4')[0]
+        dic['NCols'] = np.frombuffer(binary.read(4), '<u4')[0]
+    
+        # Read TNMR sequence and store it into dictionary
+        dic['Sequence'] = dict()
+    
+        for row in range(dic['NRows']):
+            # 28 bytes of useless data
+            np.frombuffer(binary.read(self.PSEQROW.itemsize), self.PSEQROW, count = 1)[0]
+    
+            # Read row index and use the row name as a new (sub)dictionary
+            binary.read_string()
+            row_name = binary.read_string().decode()
+            dic['Sequence'][row_name] = dict()
+    
+            for column in range(dic['NCols']):
+                # Read a column value and use it as a new key to store tables.
+                col_name = binary.read_string().decode()
+                # [value, 0D-4D table names]
+                values = [col_name]
+                for dim in range(5):
+                    values.append(binary.read_string().decode())
+                    binary.seek(4,1)
+                # whitespace
+                binary.seek(16,1)
+                dic['Sequence'][row_name][column] = values
+    
+                # [Acq row, True (1) column] has some additional data
+                if row_name == 'Acq' and col_name == '1':
+                    for i in range(6):
+                        binary.read_string()
+                    binary.seek(1,1)
+    
+        # Read TNMR tables and store them into Dictionary
+        # Old TNMR has integer N followed by N tables
+        # New TNMR has integer N followed by N*4 bytes of blank space before integer number of tables
+        dic['NTables'] = np.frombuffer(binary.read(4), '<u4')[0]
+        if np.frombuffer(binary.read(4), '<u4')[0] == 0:
+            binary.seek((dic['NTables']-1)*4,1) # -1 because we already read 4 bytes
+            dic['NTables'] = np.frombuffer(binary.read(4), '<u4')[0]
+        dic['Tables'] = dict()
+        for table in range(dic['NTables']):
+            tab_name = binary.read_string().decode()
+            dic['Tables'][tab_name] = [i for i in binary.read_string().decode().split('\r\n')]
+            binary.seek(16,1) # 16 bytes of empty space
+            binary.seek(56,1) # some data / find meaning!
+        binary.seek(2, 1)
+        return dic
+
+
+
